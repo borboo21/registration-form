62270c46e84c4d48bd0de25c355455b275fd811a
borboo21 <nikich621@gmail.com>
2024-07-08 16:01:45 +0300

    fix some issues

--- src/App.js
+++ src/App.js
@@ -11,11 +11,10 @@ const sendData = (formData) => {
 const fieldsScheme = yup.object().shape({
 	email: yup
 		.string()
-		.required('Нужно ввести почту')
-		.matches(
-			/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
+		.email(
 			'Почта должна содержать @ и домен почтового ящика, пример: "name@mail.ru"!',
-		),
+		)
+		.required('Нужно ввести почту'),
 	password1: yup
 		.string()
 		.required('Нужно ввести пароль!')
@@ -30,8 +29,11 @@ export const App = () => {
 	const {
 		register,
 		handleSubmit,
-		formState: { errors, dirtyFields },
+		getValues,
+
+		formState: { errors, dirtyFields, isValid },
 	} = useForm({
+		mode: 'onChange',
 		defaultValues: {
 			email: '',
 			password1: '',
@@ -44,19 +46,16 @@ export const App = () => {
 	const password1Error = errors.password1?.message;
 	const password2Error = errors.password2?.message;
 
-	let disableBtn = true;
-	const isDisabled = () => {
-		if (Object.keys(errors).length === 0 && Object.keys(dirtyFields).length === 3) {
-			disableBtn = false;
-			return submitBtnRef.current.focus();
-		} else {
-			disableBtn = true;
-		}
-	};
+	const emailValue = getValues('email');
+	const pass1Value = getValues('password1');
+	const pass2Value = getValues('password2');
+	console.log(emailValue, pass1Value, pass2Value);
 
+	if (isValid) {
+		submitBtnRef.current.focus();
+	}
 	return (
 		<div className={styles.main}>
-			{isDisabled()}
 			<form className={styles.form} onSubmit={handleSubmit(sendData)}>
 				<span className={styles.reg}>Регистрация</span>
 
@@ -97,7 +96,7 @@ export const App = () => {
 				<button
 					className={styles.btn}
 					type="submit"
-					disabled={disableBtn}
+					disabled={!isValid}
 					ref={submitBtnRef}
 				>
 					Зарегистрироваться
